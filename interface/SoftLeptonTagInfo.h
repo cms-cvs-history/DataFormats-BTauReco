b7fae1adcb9ceb88778e443652ed2fb585f4d0d7
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-03-06 14:25:38 +0000

    backport SoftLepton from 2.0.0-pre4

--- interface/SoftLeptonTagInfo.h
+++ interface/SoftLeptonTagInfo.h
@@ -1,32 +1,24 @@
 #ifndef DataFormats_BTauReco_SoftLeptonTagInfo_h
 #define DataFormats_BTauReco_SoftLeptonTagInfo_h
 
-//#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-//#include "DataFormats/JetReco/interface/Jet.h"
+#include "DataFormats/BTauReco/interface/RefMacros.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h" 
 #include "DataFormats/BTauReco/interface/JetTagInfo.h"
 #include "DataFormats/BTauReco/interface/TaggingVariable.h"
-#include "DataFormats/BTauReco/interface/SoftLeptonTagInfoFwd.h"
 
 namespace reco {
  
 class SoftLeptonProperties {
 public:
-    enum {
-        AXIS_CALORIMETRIC             = 0,  // use the calorimietric jet axis
-        AXIS_CHARGED_AVERAGE          = 1,  // refine jet axis using charged tracks: use a pT-weighted average of (eta, phi)
-        AXIS_CHARGED_AVERAGE_NOLEPTON = 2,  // as above, without the tagging lepton track
-        AXIS_CHARGED_SUM              = 3,  // refine jet axis using charged tracks: use the sum of tracks momentum
-        AXIS_CHARGED_SUM_NOLEPTON     = 4   // as above, without the tagging lepton track
-    };
-
-    unsigned int axisRefinement;            // if and how the jet axis is refined
     float quality;                          // lepton quality
+   
     float sip2d;                            // 2D signed impact parameter
     float sip3d;                            // 3D signed impact parameter
-    float ptRel;                            // transverse momentum wrt. jet axis
+    float ptRel;                            // transverse momentum wrt. the jet axis
+    float p0Par;                            // momentum along the jet direction, in the jet rest frame
+
     float etaRel;                           // (pseudo)rapidity along jet axis
-    float deltaR;                           // pseudoangular distance to jet axis
+    float deltaR;                           // (pseudo)angular distance to jet axis
     float ratio;                            // momentum over jet energy
     float ratioRel;                         // momentum paraller to jet axis over jet energy
 };
@@ -64,6 +56,8 @@ private:
     LeptonMap m_leptons;
 
 };
+
+DECLARE_EDM_REFS( SoftLeptonTagInfo )
 
 }
 
